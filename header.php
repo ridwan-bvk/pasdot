1d00400d89c6de0cbac8f87ef5c2517c1b0b22d2
Ridwan <64180648+ridwan-bvk@users.noreply.github.com>
2021-03-25 07:42:40 +0700

    added new file

---
+++ header.php
@@ -0,0 +1,67 @@
+<?php
+// require_once "_config/config.php";
+//  IF (!isset($_SESSION["login"])) {    
+//   echo"<script>window.location='".base_url('auth/login.php')."';</script>";
+//  }else{ 
+//   echo"<script>window.location='".base_url()."';</script>";
+//  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dasboard-Pembayaran Sampah </title>
+    <!-- Bootstrap Core CSS -->
+    <link href="<?=base_url('/_asset/css/bootstrap.min.css')?>" rel="stylesheet">
+    <link href="<?=base_url('/_asset/css/simple-sidebar.css')?>" rel="stylesheet">
+   
+</head>
+<body>
+    <script src="<?=base_url('_asset/js/jquery.js')?>"></script>
+    <script src="<?=base_url('_asset/js/bootstrap.min.js')?>"></script>
+    <div id="wrapper">
+
+<!-- Sidebar -->
+<div id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+        <li class="sidebar-brand">
+        <a href="#">
+            Start Bootstrap
+        </a>
+    </li>
+    <li>
+        <a href="#">Dashboard</a>
+    </li>
+    <li>
+        <a href="#">Shortcuts</a>
+    </li>
+    <li>
+        <a href="#">Overview</a>
+    </li>
+    <li>
+        <a href="#">Events</a>
+    </li>
+    <li>
+        <a href="#">About</a>
+    </li>
+    <li>
+        <a href="#">Services</a>
+    </li>
+    <li>
+        <a href="#">Contact</a>
+    </li>
+</ul>
+</div>
+<!-- /#sidebar-wrapper -->
+
+<!-- Page Content -->
+<div id="page-content-wrapper">
+<div class="container-fluid">
+
---
+++ iuran_pasdot.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 22, 2021 at 02:03 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iuran_pasdot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `m_user`
+--
+
+CREATE TABLE `m_user` (
+  `id_user` varchar(50) NOT NULL,
+  `nama_user` varchar(80) NOT NULL,
+  `username` varchar(40) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` enum('1','2') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `m_user`
+--
+
+INSERT INTO `m_user` (`id_user`, `nama_user`, `username`, `password`, `level`) VALUES
+('b4659fe3-8aa1-11eb-bf19-7085c22fd108', 'muhammad rizal', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', '1');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `m_user`
+--
+ALTER TABLE `m_user`
+  ADD PRIMARY KEY (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
