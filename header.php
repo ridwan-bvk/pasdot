965fd5b6b5d5faf086baebc5626d801f4b3de3fa
ridwan-bvk <readsmath@gmail.com>
2021-03-29 08:01:05 +0700

    home 290321

--- header.php
+++ header.php
@@ -1,67 +1,60 @@
 <?php
-// require_once "_config/config.php";
-//  IF (!isset($_SESSION["login"])) {    
-//   echo"<script>window.location='".base_url('auth/login.php')."';</script>";
-//  }else{ 
-//   echo"<script>window.location='".base_url()."';</script>";
-//  }
-
+ require_once "_config/config.php";
+ IF (!isset($_SESSION["login"])) {  
+       echo"<script>window.location='".base_url('auth/login.php')."';</script>";
+ }
+ 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <head>
 
-<head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>e-pasirdoton </title>
+        <!-- Bootstrap Core CSS -->
+        <link href="<?=base_url('/_asset/css/bootstrap.min.css')?>" rel="stylesheet">
+        <link href="<?=base_url('/_asset/css/simple-sidebar.css')?>" rel="stylesheet">
+    
+    </head>
+    <body>
+            <script src="<?=base_url('_asset/js/jquery.js')?>"></script>
+            <script src="<?=base_url('_asset/js/bootstrap.min.js')?>"></script>
+        <div id="wrapper">
+            <!-- Sidebar -->
+            <div id="sidebar-wrapper" class="col-xs-col-xs-offset-12">
+                <ul class="sidebar-nav ">
+                    <li class="sidebar-brand">
+                    <a href="<?=base_url('dasboard/');?>" ><span class="text-primary">e-pasirdoton</span>
+                    </a>
+                    </li>
+                    <li>
+                        <a href="<?=base_url('dasboard/');?>">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="<?=base_url('warga/');?>">Registrasi Warga</a>
+                    </li>
+                    <li>
+                        <a href="#">Iuran Warga</a>
+                    </li>
+                    <li>
+                        <a href="#">Events</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="<?=base_url('auth/logout.php')?>"><span class="text-danger">Kaluar</span></a>
+                    </li>
+                </ul>
+            </div>
+    <!-- /#sidebar-wrapper -->
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Dasboard-Pembayaran Sampah </title>
-    <!-- Bootstrap Core CSS -->
-    <link href="<?=base_url('/_asset/css/bootstrap.min.css')?>" rel="stylesheet">
-    <link href="<?=base_url('/_asset/css/simple-sidebar.css')?>" rel="stylesheet">
    
-</head>
-<body>
-    <script src="<?=base_url('_asset/js/jquery.js')?>"></script>
-    <script src="<?=base_url('_asset/js/bootstrap.min.js')?>"></script>
-    <div id="wrapper">
-
-<!-- Sidebar -->
-<div id="sidebar-wrapper">
-    <ul class="sidebar-nav">
-        <li class="sidebar-brand">
-        <a href="#">
-            Start Bootstrap
-        </a>
-    </li>
-    <li>
-        <a href="#">Dashboard</a>
-    </li>
-    <li>
-        <a href="#">Shortcuts</a>
-    </li>
-    <li>
-        <a href="#">Overview</a>
-    </li>
-    <li>
-        <a href="#">Events</a>
-    </li>
-    <li>
-        <a href="#">About</a>
-    </li>
-    <li>
-        <a href="#">Services</a>
-    </li>
-    <li>
-        <a href="#">Contact</a>
-    </li>
-</ul>
-</div>
-<!-- /#sidebar-wrapper -->
-
-<!-- Page Content -->
-<div id="page-content-wrapper">
-<div class="container-fluid">
-
+                    
